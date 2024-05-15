7b0c55ff60b3ac5e2b577e1a093035db23ad7ad4
Adrian Martinez <adrian.martinez.saldana@ibm.com>
2024-05-15 16:56:23 -0600

    Working until lession 8

---
+++ Curso JS Moderno/13-DOM/js/02-scripts.js
@@ -0,0 +1,12 @@
+const header = document.getElementsByClassName('header');
+console.log(header);
+
+const hero = document.getElementsByClassName("hero");
+console.log(hero);
+
+const contenedores = document.getElementsByClassName("contenedor");
+console.log(contenedores);
+
+
+const noExiste = document.getElementsByClassName("LOLA");
+console.log(noExiste)
---
+++ Curso JS Moderno/13-DOM/js/06-scripts.js
@@ -0,0 +1,13 @@
+const encabezado = document.querySelector('.contenido-hero h1').textContent;
+console.log(encabezado);
+
+/*
+console.log(encabezado.innerText);
+console.log(encabezado.textContent);
+console.log(encabezado.innerHTML);
+document.querySelector('.contenido-hero h1').textContent = "New Heading";
+
+*/
+
+const imagen = document.querySelector('.card img ')
+imagen.src = "img/hacer2.jpg";
